c14f6d601c3c5ed5e8d65b88e42df3ab0cea5aa1
winc_sato <ko-satou@winc.co.jp>
2020-09-29 12:15:27 +0900

    abc155 bのjavaソースを修正

--- java/20200929_abc155_b/Main.java
+++ java/20200929_abc155_b/Main.java
@@ -18,6 +18,7 @@ public class Main {
 		for(int i=0;i<n;i++){
 		    if(! (a[i] % 2 == 1 || (a[i] % 6 == 0 || a[i] % 10 == 0))){
 		        result = "DENIED";
+                break;
 		    }
 		}
 		
