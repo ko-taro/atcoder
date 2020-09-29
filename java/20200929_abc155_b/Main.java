0f5408f66192e674733bdbd496ed148688369d6e
winc_sato <ko-satou@winc.co.jp>
2020-09-29 12:21:08 +0900

    abc155 bのjavaソースを修正

--- java/20200929_abc155_b/Main.java
+++ java/20200929_abc155_b/Main.java
@@ -7,16 +7,12 @@ public class Main {
         // Nを取得
 		int n = sc.nextInt();
 		
-		//Aの各要素を取得
-		int[] a = new int[n];
+		//Aの各要素が（（奇数である）または（6の倍数または10の倍数））ではないとき、DENIEDを設定
+		int a;
+        String result = "APPROVED";
 		for(int i=0;i<n;i++){
-		    a[i] = sc.nextInt();
-		}
-		
-		//Aの要素が（（奇数である）または（6の倍数または10の倍数））ではないとき、DENIEDを設定
-		String result = "APPROVED";
-		for(int i=0;i<n;i++){
-		    if(! (a[i] % 2 == 1 || (a[i] % 6 == 0 || a[i] % 10 == 0))){
+		    a = sc.nextInt();
+            if(! (a % 2 == 1 || (a % 6 == 0 || a % 10 == 0))){
 		        result = "DENIED";
                 break;
 		    }
